85091a856447e646d087478beed0e42244ddbe5f
yeonnnnjs <39330546+yeonnnnjs@users.noreply.github.com>
2024-07-15 00:40:31 +0900

    refactor: add utils

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,16 +1,16 @@
 package baseball;
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+
+import static baseball.utils.DataUtils.convertToIntList;
+import static baseball.utils.DataUtils.generateRandomNumbers;
+import static baseball.utils.IOUtils.getResult;
+import static baseball.utils.IOUtils.validateInput;
 
 public class Application {
     public static void main(String[] args) {
         boolean playAgain = true;
-
         while (playAgain) {
             List<Integer> computerNumbers = generateRandomNumbers();
             boolean gameOver = false;
@@ -36,64 +36,5 @@ public class Application {
                 playAgain = false;
             }
         }
-    }
-
-    private static List<Integer> generateRandomNumbers() {
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-        return computer;
-    }
-
-    private static void validateInput(String input) {
-        if (input.length() != 3) {
-            throw new IllegalArgumentException();
-        }
-        Set<Character> charSet = new HashSet<>();
-        for (char ch : input.toCharArray()) {
-            if (!Character.isDigit(ch) || ch == '0' || !charSet.add(ch)) {
-                throw new IllegalArgumentException();
-            }
-        }
-    }
-
-    private static List<Integer> convertToIntList(String input) {
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            numbers.add(input.charAt(i) - '0');
-        }
-        return numbers;
-    }
-
-    private static Boolean getResult(List<Integer> computerNumbers, List<Integer> userNumbers) {
-        int strikes = 0;
-        int balls = 0;
-        String result = "";
-
-        for (int i = 0; i < 3; i++) {
-            if (userNumbers.get(i).equals(computerNumbers.get(i))) {
-                strikes++;
-            } else if (computerNumbers.contains(userNumbers.get(i))) {
-                balls++;
-            }
-        }
-
-        if (strikes == 3) {
-            result = "3스트라이크";
-            System.out.println(result);
-            return true;
-        }
-
-        if (strikes == 0 && balls == 0) {
-            result = "낫싱";
-        } else {
-            result = balls + "볼 " + strikes + "스트라이크";
-        }
-        System.out.println(result);
-        return false;
     }
 }
