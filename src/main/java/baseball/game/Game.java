83d469f17cb75298d723930b7c9f0a529801ccb5
hyunzzii <yhynym@gmail.com>
2024-07-14 18:24:41 +0900

    feat: Game 기능 구현

---
+++ src/main/java/baseball/game/Game.java
@@ -0,0 +1,30 @@
+package baseball.game;
+
+import baseball.domain.Answer;
+import baseball.domain.BaseballNumber;
+import baseball.domain.Computer;
+import baseball.domain.EndNumber;
+import baseball.view.View;
+
+public class Game {
+    private Computer computer;
+
+    public Game(){
+        this.computer = new Computer();
+    }
+
+    public void start(){
+        View.init();
+        Answer answer;
+        do {
+            BaseballNumber baseballNumber = new BaseballNumber(View.input());
+            answer = computer.calculate(baseballNumber);
+            View.showAnswer(answer.getString());
+        }while(answer.isStatusWRONG());
+    }
+
+    public boolean end(){
+        EndNumber endNumber = new EndNumber(View.showEnd());
+        return endNumber.isEnd();
+    }
+}
