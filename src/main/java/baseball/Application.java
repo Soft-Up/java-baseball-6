b7801be537c7b72d09460cbe42663cf6a5b1b7e0
hyunzzii <yhynym@gmail.com>
2024-07-14 18:25:24 +0900

    feat: Application main 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.game.Game;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        boolean end = false;
+        while (!end) {
+            Game game = new Game();
+            game.start();
+            end = game.end();
+        }
     }
 }
