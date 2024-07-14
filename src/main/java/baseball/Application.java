b582d2468608488676d9aefd71c37630822ff342
hyunzzii <yhynym@gmail.com>
2024-07-14 18:45:12 +0900

    style: Application while -> do while로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,10 +6,10 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         boolean end = false;
-        while (!end) {
+        do {
             Game game = new Game();
             game.start();
             end = game.end();
-        }
+        }while(!end);
     }
 }
