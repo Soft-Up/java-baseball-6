432a434aba8345160934d8c504f0c3219603e2c8
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-08-08 02:52:49 +0900

    refactor: 기능별로 메소드 분리 및 기능 명세서 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,85 +8,133 @@ import java.util.List;
 import java.util.Objects;
 
 public class Application {
+    public static final int NUM_LENGTH = 3;
+
     public static void main(String[] args) {
-        int option = 0;
+        int option;
         System.out.println("숫자 야구 게임을 시작합니다.");
+
         do {
-            // 랜덤 숫자 생성
-            List<Integer> computer = new ArrayList<>();
-
-            while (computer.size() < 3) {
-                int randomNumber = Randoms.pickNumberInRange(1, 9);
-
-                if (!computer.contains(randomNumber)) {
-                    computer.add(randomNumber);
-                }
-            }
-
-            // System.out.println(computer);
-
-            // 숫자를 맞출 때까지 반복
-            while (true) {
-                int strike = 0;
-                int ball = 0;
-
-                // 숫자 입력
-                System.out.print("숫자를 입력해주세요 : ");
-                String num = Console.readLine();
-
-                if (num.length() != 3)
-                    throw new IllegalArgumentException("3자리의 숫자를 입력하여야 합니다.");
-
-                // 배열에 숫자 배열에 저장
-                List<Integer> numList = new ArrayList<>(3);
-
-                for (int i =0; i < 3; i ++) {
-                    char ch = num.charAt(i);
-
-                    if (Character.isDigit(ch)) {
-                        numList.add(Character.getNumericValue(ch));
-                    } else {
-                        throw new IllegalArgumentException("숫자만 입력해야 합니다.");
-                    }
-                }
-
-                // System.out.println("num: " + numList);
-
-                for (int i = 0; i < 3; i ++) {
-                    int checkingNum = numList.get(i);
-                    if (Objects.equals(checkingNum, computer.get(i))) {
-                        strike ++;
-                    }
-                    else if (computer.contains(checkingNum)) {
-                        ball ++;
-                    }
-                }
-
-                if (strike == 3) {
-                    System.out.println(strike + "스트라이크");
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    break;
-                }
-                else if (strike == 0 && ball == 0) {
-                    System.out.println("낫싱");
-                }
-                else if (strike == 0) {
-                    System.out.println(ball + "볼");
-                }
-                else if (ball == 0) {
-                    System.out.println(strike + "스트라이크");
-                }
-                else {
-                    System.out.println(ball + "볼 " + strike + "스트라이크");
-                }
-            }
-
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            option = Integer.parseInt(Console.readLine());
-
-            if (option != 1 && option != 2)
-                throw new IllegalArgumentException("option은 1 또는 2여야 합니다.");
-
+            List<Integer> computerNumbers = generateComputerNumbers();
+            playGame(computerNumbers);
+            option = getOption();
         } while (option == 1);
     }
+
+    private static List<Integer> generateComputerNumbers () {
+        List<Integer> computerNumbers = new ArrayList<>();
+
+        while (computerNumbers.size() < NUM_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+        }
+
+        return computerNumbers;
+    }
+
+    private static void playGame(List<Integer> computerNumbers) {
+        while (true) {
+            List<Integer> userNumbers = inputNumber();
+
+            int strike = countStrike(computerNumbers, userNumbers);
+            int ball = countBall(computerNumbers, userNumbers);
+
+            System.out.println(getResult(strike, ball));
+
+            if (isSuccess(strike)) {
+                break;
+            }
+
+        }
+
+    }
+
+    private static List<Integer> inputNumber () {
+        System.out.print("숫자를 입력해주세요 : ");
+        String num = Console.readLine();
+
+        if (num.length() != NUM_LENGTH) {
+            throw new IllegalArgumentException(NUM_LENGTH + "자리의 숫자를 입력하여야 합니다.");
+        }
+
+        List<Integer> userNumbers = new ArrayList<>();
+
+        for (int i =0; i < NUM_LENGTH; i ++) {
+            char ch = num.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                userNumbers.add(Character.getNumericValue(ch));
+            } else {
+                throw new IllegalArgumentException("숫자만 입력해야 합니다.");
+            }
+        }
+        return userNumbers;
+    }
+
+    private static int countStrike (List<Integer> computerNumbers, List<Integer> userNumbers) {
+        int strike = 0;
+
+        for (int i = 0; i < NUM_LENGTH; i ++) {
+            if (computerNumbers.get(i).equals(userNumbers.get(i))) {
+                strike++;
+            }
+        }
+
+        return strike;
+    }
+
+    private static int countBall (List<Integer> computerNumbers, List<Integer> userNumbers) {
+        int ball = 0;
+
+        for (int i = 0; i < NUM_LENGTH; i ++) {
+            int userNum = userNumbers.get(i);
+            if (computerNumbers.contains(userNum) && !computerNumbers.get(i).equals(userNum)) {
+                ball++;
+            }
+        }
+
+        return ball;
+    }
+
+    private static String getResult(int strike, int ball) {
+
+        if (strike == 0 && ball == 0) {
+            return "낫싱";
+        }
+        else if (strike == 0) {
+            return ball + "볼";
+        }
+        else if (ball == 0) {
+            return strike + "스트라이크";
+        }
+        else {
+            return ball + "볼 " + strike + "스트라이크";
+        }
+    }
+
+    private static boolean isSuccess (int strike) {
+
+        if (strike == NUM_LENGTH) {
+            System.out.println(NUM_LENGTH + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+
+        return false;
+    }
+
+    private static int getOption() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        int option = Integer.parseInt(Console.readLine());
+
+        if (option != 1 && option != 2) {
+            throw new IllegalArgumentException("option은 1 또는 2여야 합니다.");
+        }
+
+        return option;
+    }
+
 }
